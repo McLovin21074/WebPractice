68454e16a1da1a8f6087a6b87e870dbf01d114be
Alex A <alexanderrarapov@gmail.com>
2025-04-22 21:15:44 +0500

    lesson14

--- catalog.js
+++ catalog.js
@@ -2,7 +2,7 @@ import { Catalog } from "./src/components/catalog.js"
 
 const renderPostItem = item => `
     <a  
-        href="posts/${item.id}"
+        href="post.html?id=${item.id}"
         class="post-item"
     >
         <span class="post-item__title">
@@ -15,13 +15,16 @@ const renderPostItem = item => `
     </a>
 `
 
-const getPostItems = ({ limit, page }) => {
-    return fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`)
-        .then(async res => {
-            const total = +res.headers.get('x-total-count')
-            const items = await res.json()
-            return { items, total }
-        })
+const getPostItems = async ({ limit, page }) => {
+    try {
+        const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`)
+        const total = +res.headers.get('x-total-count')
+        const items = await res.json()
+        return { items, total }
+    } catch (error) {
+        console.error(error)
+        return { items: [], total: 0 }
+    }
 }
 
 const renderPhotoItem = item => `
@@ -40,13 +43,16 @@ const renderPhotoItem = item => `
     </a>
 `
 
-const getPhotoItems = ({ limit, page }) => {
-    return fetch(`https://jsonplaceholder.typicode.com/photos?_limit=${limit}&_page=${page}`)
-        .then(async res => {
-            const total = +res.headers.get('x-total-count')
-            const items = await res.json()
-            return { items, total }
-        })
+const getPhotoItems = async ({ limit, page }) => {
+    try {
+        const res = await fetch(`https://jsonplaceholder.typicode.com/photos?_limit=${limit}&_page=${page}`)
+        const total = +res.headers.get('x-total-count')
+        const items = await res.json()
+        return { items, total }
+    } catch (error) {
+        console.error(error)
+        return { items: [], total: 0 }
+    }
 }
 
 const init = () => {
